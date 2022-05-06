7907b44d769f12c9a78b2f52500261afa40bae51
Olah Antonio <antonioolah@yahoo.com>
2022-05-06 16:51:25 +0300

    Added Euler's number algorithm

--- New/BASED/src/main/java/Algorithms/EMain.java
+++ New/BASED/src/main/java/Algorithms/EMain.java
@@ -1,7 +1,33 @@
 package Algorithms;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class EMain {
-    public static void main(String[] args){
-        
+    static int precision;
+
+    public EMain(int no)
+    {
+        precision=no+1;
+    }
+
+    public BigDecimal calculateE()
+    {
+        BigDecimal E=new BigDecimal(0).setScale(precision,RoundingMode.HALF_EVEN);
+        BigDecimal factorialStep=new BigDecimal(0).setScale(precision,RoundingMode.HALF_EVEN);;
+        BigDecimal one=new BigDecimal(1).setScale(precision,RoundingMode.HALF_EVEN);
+        for(int i=0;i<precision-1;i++)
+        {
+            factorialStep=one.divide(factorial(i),precision,RoundingMode.HALF_EVEN).setScale(precision,RoundingMode.HALF_EVEN);
+            E=E.add(factorialStep).setScale(precision,RoundingMode.HALF_EVEN);
+        }
+        return E;
+    }
+    public static BigDecimal factorial(int n){
+        BigDecimal value = BigDecimal.valueOf(1).setScale(precision, RoundingMode.HALF_EVEN);;
+        for(int i = 1; i <=n; i++){
+            value = value.multiply(BigDecimal.valueOf(i)).setScale(precision,RoundingMode.HALF_EVEN);
+        }
+        return value;
     }
 }
